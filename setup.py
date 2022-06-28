823d54c6b686c9e706fa92beefb4b716fa74f44f
weibeu <deepakrajko14@gmail.com>
2022-06-28 20:31:25 +0530

    Include packaging configurations

---
+++ setup.py
@@ -0,0 +1,41 @@
+import re
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def __get_version():
+    with open("vtu_captcha/__init__.py") as file:
+        return re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', file.read(), re.MULTILINE).group(1)
+
+
+def __get_requirements():
+    with open("requirements.txt") as file:
+        return file.readlines()
+
+
+requirements = __get_requirements()
+version = __get_version()
+setuptools.setup(
+    name="vtu-captcha-solver",
+    version=version,
+    author="Weibeu",
+    author_email="deepakrajko14@gmail.com",
+    description="VTU captcha solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/weibeu/vtu-captcha-solver",
+    project_urls={
+        "Bug Tracker": "https://github.com/weibeu/vtu-captcha-solver/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
---
+++ vtu_captcha/__init__.py
@@ -0,0 +1,4 @@
+from vtu_captcha.captcha_solver import VTUCaptchaSolver
+
+
+__version__ = "1.0.0"
