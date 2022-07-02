b46d0021b2581c22cf90fc460500e3fbce399d71
weibeu <deepakrajko14@gmail.com>
2022-07-03 04:54:07 +0530

    Include few more captcha tests

--- tests/test_captcha_solver.py
+++ tests/test_captcha_solver.py
@@ -3,21 +3,26 @@ from vtu_captcha.captcha_solver import VTUCaptchaSolver
 import unittest
 
 
-CAPTCHA_FILEPATH = "tests/test_captcha.png"
-CAPTCHA_VALUE = "zsWmKR"
+CAPTCHAS_BASE_PATH = "tests/test_captchas/"
 
 
-def get_captcha_bytes():
-    with open(CAPTCHA_FILEPATH, "rb") as cf:
+def get_captcha_bytes(filename):
+    with open(CAPTCHAS_BASE_PATH + filename, "rb") as cf:
         return cf.read()
 
 
 class TestCaptchaSolver(unittest.TestCase):
 
-    def setUp(self):
-        captcha_bytes = get_captcha_bytes()
-        self.solver = VTUCaptchaSolver(captcha_bytes)
+    def _test_captcha(self, filepath, correct_captcha):
+        captcha_bytes = get_captcha_bytes(filepath)
+        captcha = VTUCaptchaSolver.solve_from_captcha_bytes(captcha_bytes)
+        self.assertEqual(captcha, correct_captcha)
 
-    def test_captcha_solver(self):
-        captcha = self.solver.solve()
-        self.assertEqual(captcha, CAPTCHA_VALUE)
+    def test_zero_rotated(self):
+        self._test_captcha("test_captcha_zero_rot.png", "zsWmKR")
+
+    def test_positive_rotated(self):
+        self._test_captcha("test_captcha_pos_rot.png", "CHCUpV")
+
+    def test_negative_rotated(self):
+        self._test_captcha("test_captcha_neg_rot.png", "7FLrkW")
